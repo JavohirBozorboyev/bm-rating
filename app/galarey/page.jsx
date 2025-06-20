bca8813c771fa5140320159aa704df407ae60e17
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2025-06-20 11:35:04 +0500

    galareya sahifasi

--- app/galarey/page.jsx
+++ app/galarey/page.jsx
@@ -2,6 +2,7 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
+// import IMG1 from "../../public/IMG_2262.JPG";
 
 const page = () => {
   return (
@@ -23,57 +24,75 @@ const Cards = () => {
     <div className="absolute inset-0 z-10" ref={containerRef}>
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1635373670332-43ea883bb081?q=80&w=2781&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={"/IMG_2262.JPG"}
         alt="Example image"
         rotate="6deg"
         top="20%"
         left="25%"
-        className="w-36 md:w-56"
+        className="w-40 md:w-96"
       />
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1576174464184-fb78fe882bfd?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={"/IMG_2442.JPG"}
         alt="Example image"
         rotate="12deg"
         top="45%"
         left="60%"
-        className="w-24 md:w-48"
+        className="w-40 md:w-96"
       />
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1503751071777-d2918b21bbd9?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={"/IMG_2522.JPG"}
         alt="Example image"
         rotate="-6deg"
         top="20%"
         left="40%"
-        className="w-52 md:w-80"
+        className="w-52 md:w-96"
       />
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?q=80&w=2609&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={"/IMG_3549.JPG"}
         alt="Example image"
         rotate="8deg"
         top="50%"
         left="40%"
-        className="w-48 md:w-72"
+        className="w-48 md:w-96"
       />
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1602212096437-d0af1ce0553e?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={"/IMG_3563.JPG"}
         alt="Example image"
         rotate="18deg"
         top="20%"
         left="65%"
-        className="w-40 md:w-64"
+        className="w-44 md:w-96"
       />
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1622313762347-3c09fe5f2719?q=80&w=2640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={"/IMG_3685.JPG"}
+        alt="Example image"
+        rotate="-3deg"
+        top="50%"
+        left="20%"
+        className="w-44 md:w-60"
+      />
+      <Card
+        containerRef={containerRef}
+        src={"/IMG_8188.JPG"}
+        alt="Example image"
+        rotate="-3deg"
+        top="35%"
+        left="25%"
+        className="w-36 md:w-80"
+      />
+      <Card
+        containerRef={containerRef}
+        src={"/IMG_8339.JPG"}
         alt="Example image"
         rotate="-3deg"
         top="35%"
         left="55%"
-        className="w-24 md:w-48"
+        className="w-40 md:w-80"
       />
     </div>
   );
